7f290f1983018957a5310da2bba28394ef1e0d29
zahidmiran <zahidmiron599@gmail.com>
2023-09-14 19:59:49 +0600

    profile route added successfully

---
+++ src/app/modules/profile/profile.controllers.ts
@@ -0,0 +1,22 @@
+import { User } from '@prisma/client';
+import { RequestHandler } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { ProfileServices } from './profile.services';
+
+const getUserProfile: RequestHandler = catchAsync(async (req, res) => {
+  const userId = req.user?.userId;
+  const result = await ProfileServices.getUserProfile(userId);
+
+  sendResponse<User | null>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User profile fetched successfully',
+    data: result,
+  });
+});
+
+export const ProfileControllers = {
+  getUserProfile,
+};
